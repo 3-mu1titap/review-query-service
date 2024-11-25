f2a495d841747988772f185a22ab712acd141860
Seong-Tae-Kim <rlatjdxo6462@gmail.com>
2024-11-25 17:21:03 +0900

    first commit

---
+++ src/main/java/com/multitap/reviewQuery/ReviewQueryApplication.java
@@ -0,0 +1,13 @@
+package com.multitap.reviewQuery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewQueryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multitap/reviewQuery/ReviewQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multitap.reviewQuery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewQueryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

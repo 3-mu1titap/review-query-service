c7ba4bddbad601ef1ead3bb312931650409048e7
Seong-Tae-Kim <rlatjdxo6462@gmail.com>
2024-12-05 12:10:37 +0900

    Infra

--- src/main/java/com/multitap/reviewQuery/reviewQuery/feignClient/MentoringServiceFeignClient.java
+++ src/main/java/com/multitap/reviewQuery/reviewQuery/feignClient/MentoringServiceFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:9001", name = "mentoring-query-service")
+@FeignClient(url = "http://api.adaptors.site/mentoring-query-service", name = "mentoring-query-service")
 public interface MentoringServiceFeignClient {
 
     // mentoring-query-service 의 멘토 uuid 조회 api 호출

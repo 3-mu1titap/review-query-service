f663d2ca4ae76fb15b4e7b6cbac5dba46c99f822
Seong-Tae-Kim <rlatjdxo6462@gmail.com>
2024-12-09 11:03:19 +0900

    Refac: 반환값 구조 수정

--- src/main/java/com/multitap/reviewQuery/reviewQuery/feignClient/MemberServiceFeignClient.java
+++ src/main/java/com/multitap/reviewQuery/reviewQuery/feignClient/MemberServiceFeignClient.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-//@FeignClient(url = "http://localhost:8083", name = "member-query-service")
-@FeignClient(url = "http://api.adaptors.site/member-query-service", name = "member-query-service")
+@FeignClient(url = "http://localhost:8083", name = "member-query-service")
+//@FeignClient(url = "http://api.adaptors.site/member-query-service", name = "member-query-service")
 public interface MemberServiceFeignClient {
 
     // mentoring-query-service 의 멘토 uuid 조회 api 호출
